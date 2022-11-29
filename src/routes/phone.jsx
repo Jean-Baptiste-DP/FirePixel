ecfa6e89b44e12029043bae5cb48754b8097e173
nbert71 <nicobert2205@gmail.com>
2022-11-29 15:56:11 +0100

    add warning messages based on screen width

--- src/routes/phone.jsx
+++ src/routes/phone.jsx
@@ -1,9 +1,15 @@
 export default function Phone() {
     return (
       <>
-        <h1 className="text-3xl text-blue-700">
-          Hello from Phone !
-        </h1>
+        <div className="hidden sm:block">
+          Cette page n'est pas censée être consultée depuis un écran aussi grand !
+        </div>
+
+        <div className="block sm:hidden">
+          <h1 className="text-3xl text-blue-700">
+            Hello from Phone !
+          </h1>
+        </div>
       </>
     );
   }
--- src/routes/screen.jsx
+++ src/routes/screen.jsx
@@ -1,9 +1,15 @@
 export default function Screen() {
   return (
     <>
-      <h1 className="text-3xl text-red-700">
-        Hello from Screen !
-      </h1>
-    </>
+        <div className="block sm:hidden">
+          Cette page n'est pas censée être consultée depuis un écran aussi petit !
+        </div>
+
+        <div className="hidden sm:block">
+          <h1 className="text-3xl text-red-700">
+            Hello from Screen !
+          </h1>
+        </div>
+      </>
   );
 }

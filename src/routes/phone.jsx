3ded9302ab78856b7b6a9a0b8074b2d06b3ed410
Tuncay Bilgi <tuncay.bilgi39@gmail.com>
2023-01-05 19:19:15 +0100

    Update phone.jsx

--- src/routes/phone.jsx
+++ src/routes/phone.jsx
@@ -6,7 +6,8 @@ import React,{useState,useEffect} from 'react';
 
 
 const colordict = {0:'bg-emerald-500', 1 : 'bg-green-400', 3 : 'bg-yellow-300', 4 : 'bg-amber-500', 2 : 'bg-lime-200', 5 : 'bg-red-500', 6 : 'bg-rose-400', 7 : 'bg-cyan-300', 8 : 'bg-sky-500', 10 : 'bg-purple-500', 9 : 'bg-indigo-300', 11 : 'bg-amber-800', 12 : 'bg-black', 13 : 'bg-slate-400', 14 : 'bg-slate-200', 15 : 'bg-white'};
-const namedict = {0:'Emerald', 1 : 'Green', 3 : 'Yellow', 4 : 'Amber', 2 : 'Lime', 5 : 'Red', 6 : 'Rose', 7 : 'Cyan', 8 : 'Sky', 10 : 'Purple', 9 : 'Indigo', 11 : 'Brown', 12 : 'Black', 13 : 'Dark Grey', 14 : 'Light Grey', 15 : 'White'};
+const namedict = {0:'Emerald', 1 : 'Green', 3 : 'Yellow', 4 : 'Amber', 2 : 'Lime', 5 : 'Red', 6 : 'Rose', 7 : 'Cyan', 8 : 'Sky', 10 : 'Purple', 9 : 'Indigo', 11 : 'Brown', 12 : 'Black', 13 : 'Dark Gray', 14 : 'Light Gray', 15 : 'White'};
+const regles = "Dessinez ce que vous voulez sur l'écran! Dans l'onglet color, choissisez une couleur avec laquelle dessiner,puis déplacez le joystick pour appliquer la couleur. Si vous avez décidé de faire du PixelArt, 'Apply' vous permet de placer les pixels un à un.";
 
 export default function Phone({grid,cursor,sendJsonMessage}) {
 
@@ -50,7 +51,7 @@ for (let i=0;i<16;i++){
     setHelpState(prevhelpState => ({...prevhelpState,Display : '-z-10 opacity-0', State : 'off'}));
   }
     else {
-      setHelpState(prevhelpState => ({...prevhelpState , Display:'z-10',State : 'on'}));
+      setHelpState(prevhelpState => ({...prevhelpState , Display:'z-20',State : 'on'}));
     }
   }
 
@@ -59,7 +60,7 @@ for (let i=0;i<16;i++){
     setPaletteState(prevPaletteState => ({...prevPaletteState,Display : '-z-10 opacity-0', State : 'off'}));
   }
     else {
-      setPaletteState(prevPaletteState => ({...prevPaletteState , Display:'z-10',State : 'on'}));
+      setPaletteState(prevPaletteState => ({...prevPaletteState , Display:'z-20',State : 'on'}));
     }
   }
 
@@ -72,8 +73,7 @@ for (let i=0;i<16;i++){
   
   }
 
-  const regles = "Dessinez ce que vous voulez sur l'écran! Dans l'onglet color, choissisez une couleur avec laquelle dessiner,puis déplacer le joystick pour appliquer la couleur. Si vous avez décidé de faire du PixelArt, appuyer sur 'Apply' pour placer les pixels un à un.";
-
+  
 
   // Websockets 
   // let socket = new WebSocket('ws://localhost:10406');
@@ -111,14 +111,16 @@ for (let i=0;i<16;i++){
         <div className="block sm:hidden">
 
           <div className={`grid grid-cols-1 place-items-center absolute h-full w-full ${helpState.Display} transition duration-300 `}>
-            <button className="h-3/4 w-2/3  bg-dark rounded-lg p-4 " onClick={helpOnClick}>
+            <button className='absolute h-full w-full bg-gray-300 opacity-70 ' onClick={helpOnClick} ></button>
+            <button className="relative h-3/4 w-2/3  bg-dark rounded-lg p-4 " onClick={helpOnClick}>
               <p className="text-white text-xl">{regles}</p>
             </button>
           </div>
-
+          
           <div className={`grid place-items-center  absolute h-full w-full ${paletteState.Display} transition duration-300 `}>
+            <button className='absolute h-full w-full bg-gray-300 opacity-70 ' onClick={colorOnClick} ></button>
             <div className='m-12'/>
-            <div className="grid grid-rows-4 h-1/2 w-[92%] place-items-center  bg-dark rounded-lg p-4 gap-2 -translate-y-1 drop-shadow-lg">
+            <div className="relative grid grid-rows-4 h-1/2 w-[92%] place-items-center  bg-dark rounded-lg p-4 gap-2 -translate-y-1 drop-shadow-lg">
               <div className='grid grid-cols-5 gap-4'>
                 {rows.slice(0,5)}
               </div>
@@ -158,7 +160,7 @@ for (let i=0;i<16;i++){
             <div className="my-1 grid grid-rows-1 bg-primary rounded-lg h-1/3 place-items-center">
             <HomeJoystick websocket={sendJsonMessage} color={pixel} pixelArt={pixelArt}/>
             </div>
-              <div className='h-[8%] p-2 grid grid-cols-3 place-items-center gap-5 relative z-10 bg-primary rounded-lg'>
+              <div className='h-[8%] p-2 grid grid-cols-3 place-items-center gap-5 relative z-20 bg-primary rounded-lg'>
                 <Button text='Help' onClick={helpOnClick} state={helpState.State}/>
                 <Button text='Color' onClick={colorOnClick} state = {paletteState.State}/>
                 <Button text='Apply' onClick={() => applyOnClick(sendJsonMessage,pixel)}/>

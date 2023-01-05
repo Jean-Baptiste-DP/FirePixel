aadc2f691f78a8ec1d9085bf5fd7fd9304188043
Jean-Baptiste-DP <jb.durandpierre@gmail.com>
2023-01-05 18:05:54 +0100

    🐛 dotted and continuous bug + corner bug

--- src/components/phone/homeJoystick.jsx
+++ src/components/phone/homeJoystick.jsx
@@ -2,37 +2,33 @@ import { Joystick } from 'react-joystick-component';
 
 export default function HomeJoystick({websocket,color,pixelArt}) {
 
-  var x = 0.5;
-  var y = 0.5;
+  var x = 0;
+  var y = 0;
 
   function handleMove(event) {
-    var d_x = event.x;
-    var d_y = event.y;
     
-    x+= d_x*2;
-    y+= d_y*2;
+    x+= event.x;
+    y+= event.y;
 
     if(Math.abs(x)>=1 || Math.abs(y)>=1){
-      var x_int = Math.trunc(x)
-      var y_int = Math.trunc(y)
-
-      x-= x_int
-      y-= y_int
 
       websocket(
             {
               req : 'move',
-              x : x_int,
-              y : y_int
+              x : Math.trunc(x),
+              y : Math.trunc(y)
             });
-        
+
+      x= 0
+      y= 0
+      
       if (pixelArt == true) {
-      websocket(
-              {
-                req : 'chgColor',
-                color : color
-              });
-            }
+        websocket(
+                {
+                  req : 'chgColor',
+                  color : color
+                });
+      }
     }
   }
 
